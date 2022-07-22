c4119ccaf6b138dbbcf23b3d73634b529ef758de
bidkhonei <hassan317zahedi@gmail.com>
2022-07-22 10:23:34 +0330

    initial commit2

--- src/main/java/service/printer.java
+++ src/main/java/service/printer.java
@@ -1,9 +1,12 @@
 package service;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class printer {
     public void printMenu(String[] Menu) {
         for (int i = 0; i < Menu.length; i++) {
             System.out.println(Menu[i]);
+            System.out.println("hhhhhh");
         }
     }
 }
